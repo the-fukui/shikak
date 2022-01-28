7785503b6a9b44f960574b3fc54bb263f0228060
the-fukui <account@the-fukui.com>
2022-01-29 08:15:31 +0900

    prisma

--- env.sh
+++ env.sh
@@ -3,3 +3,4 @@
 # gcloud config set project [project-id] && \
 # gcloud secrets versions access latest --secret [secret-name] >.env.local && \
 ls -a .env* | xargs -I {} ln -sf ../../{} packages/web/{}
+ls -a .env* | xargs -I {} ln -sf ../../{} packages/db/{}
